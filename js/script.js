96310929d18cb6a28cee6b7de286a1f43845a6d6
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2023-09-12 00:02:12 +0200

    controllo numerico e alert di errore

--- js/script.js
+++ js/script.js
@@ -1,53 +1,120 @@
 $(document).ready(function () {
-    
-    somma = function (a, b) {
-        var a = $('#primo').val();
-        var b = $('#secondo').val();
+    $('#alert').css('display', 'block');
+
+    // ADDIZIONE
+    somma = function(a, b) {
+        // CATTURO I VALORI INSERITI DALL'UTENTE
+        a = $('#primo').val();
+        b = $('#secondo').val();
         
-        somma = parseInt(a) + parseInt(b);
-        //alert(somma);
-        
-        $('#risultato').val(somma);
-    }
-    
-    sottrai = function (a, b) {
-        var a = $('#primo').val();
-        var b = $('#secondo').val();
-        
-        sottrazione = parseInt(a) - parseInt(b);
-        //alert(sottrazione);
-        
-        $('#risultato').val(sottrazione);
-    }
-    
-    moltiplica = function (a, b) {
-        var a = $('#primo').val();
-        var b = $('#secondo').val();
-        
-        moltiplicazione = parseInt(a) * parseInt(b);
-        //alert(moltiplicazione);
-        
-        $('#risultato').val(moltiplicazione);
-    }
-    
-    dividi = function (a, b) {
-        var a = $('#primo').val();
-        var b = $('#secondo').val();
-        
-        divisione = parseInt(a) / parseInt(b);
-        //alert(divisione);
-        
-        $('#risultato').val(divisione);
-    }
-    
-    azzera = function (result) {
-        var result = $('#risultato').val();
-        
-        if (result !== '') {
-            $('#primo').val('');
-            $('#secondo').val('');
-            $('#risultato').val('');
+        // SE ENTRAMBI I VALORI SONO DEI NUMERI
+        if ($.isNumeric(a) && $.isNumeric(b)) {
+            // ESEGUO L'ADDIZIONE
+            addizione = parseInt(a) + parseInt(b);
+            
+            // INSERISCO IL RISULTATO SUL CAMPO SELEZIONATO
+            $('#risultato').val(addizione);
+            // ALTRIMENTI
+        } else {
+            // INSERISCO UN ALERT SUL CAMPO SELEZIONATO
+            $('#alert').html(`
+            <div class="alert alert-danger" role="alert">
+            Devi inserire un valore numerico.
+            </div>
+            `);
+            // E SVUOTO I CAMPI INPUT
+            $('input').val('');
         }
+        
+    }
+    
+    // SOTTRAZIONE
+    sottrai = function(a, b) {
+        // CATTURO I VALORI INSERITI DALL'UTENTE
+        a = $('#primo').val();
+        b = $('#secondo').val();
+        
+        // SE ENTRAMBI I VALORI SONO DEI NUMERI
+        if ($.isNumeric(a) && $.isNumeric(b)) {
+            // ESEGUO LA SOTTRAZIONE
+            sottrazione = parseInt(a) - parseInt(b);
+            
+            // INSERISCO IL RISULTATO SUL CAMPO SELEZIONATO
+            $('#risultato').val(sottrazione);
+            // ALTRIMENTI
+        } else {
+            // INSERISCO UN ALERT SUL CAMPO SELEZIONATO
+            $('#alert').html(`
+            <div class="alert alert-danger" role="alert">
+            Devi inserire un valore numerico.
+            </div>
+            `);
+            // E SVUOTO I CAMPI IN INPUT
+            $('input').val('');
+        }
+        
+    }
+    
+    // MOLTIPLICAZIONE
+    moltiplica = function(a, b) {
+        // CATTURO I VALORI INSERITI DALL'UTENTE
+        a = $('#primo').val();
+        b = $('#secondo').val();
+        
+        // SE ENTRAMBI I VALORI SONO DEI NUMERI
+        if ($.isNumeric(a) && $.isNumeric(b)) {
+            // ESEGUO LA MOLTIPLICAZIONE
+            moltiplicazione = parseInt(a) * parseInt(b);
+            
+            // INSERISCO IL RISULTATO SUL CAMPO SELEZIONATO
+            $('#risultato').val(moltiplicazione);
+            // ALTRIMENTI
+        } else {
+            // INSERISCO UN ALERT SUL CAMPO SELEZIONATO
+            $('#alert').html(`
+            <div class="alert alert-danger" role="alert">
+            Devi inserire un valore numerico.
+            </div>
+            `);
+            // E SVUOTO I CAMPI IN INPUT
+            $('input').val('');
+        }
+        
+    }
+    
+    // DIVISIONE
+    dividi = function(a, b) {
+        // CATTURO I VALORI INSERITI DALL'UTENTE
+        a = $('#primo').val();
+        b = $('#secondo').val();
+        
+        // SE ENTRAMBI I VALORI SONO DEI NUMERI
+        if ($.isNumeric(a) && $.isNumeric(b)) {
+            // ESEGUO LA DIVIZIONE
+            divisione = parseInt(a) / parseInt(b);
+            
+            // INSERISCO IL RISULTATO SUL CAMPO SELEZIONATO
+            $('#risultato').val(divisione);
+            // ALTRIMENTI
+        } else {
+            // INSERISCO UN ALERT SUL CAMPO SELEZIONATO
+            $('#alert').html(`
+            <div class="alert alert-danger" role="alert">
+            Devi inserire un valore numerico.
+            </div>
+            `);
+            // E SVUOTO I CAMPI IN INPUT
+            $('input').val('');
+        }
+        
+    }
+    
+    // AZZERAMENTO
+    azzera = function() {
+        // SVUOTO I CAMPI IN INPUT
+        $('input').val('');
+        // SOSTITUISCO L'ALERT CON UN DIV VUOTO
+        $('#alert').html(`<div></div>`);
     }
     
 });
